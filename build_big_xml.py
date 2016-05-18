a79a0db7a0aaac81a7748fc04bc0cfcf181d6c3a
Ben Bales <bbbales2@gmail.com>
2016-05-18 15:22:30 -0700

    I just needed to back up these scraping scripts

---
+++ build_big_xml.py
@@ -0,0 +1,141 @@
+#%%
+import collections
+import json
+import nltk
+import numpy
+import gensim
+import os
+import itertools
+import random
+import re
+import time
+import matplotlib.pyplot as plt
+import pattern.en, pattern.search
+import enchant
+import sklearn.cluster
+import scipy.cluster.hierarchy
+import stemming.porter2
+from miningTools import stemSentence
+import sys
+
+import sqlalchemy, sqlalchemy.orm
+
+import mldb2
+
+session, engine = mldb2.getSession('/home/bbales2/scraping/webpage/db/sentences.document.sqlite.mldb2.db')
+
+#%%
+class LoaderStopwords(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        stemmedStops = stemSentence(' '.join(nltk.corpus.stopwords.words("english")))
+        notword = re.compile(r'[^\w -]')
+        for sentence in itertools.chain(*[session.query(cls).order_by(cls.id).yield_per(100) for cls in self.clss]):
+            stemmed = stemSentence(sentence.string)
+            filtered = [word for word in stemmed if word not in stemmedStops and len(notword.sub('', word)) > 0 and len(word) < 75]
+            yield filtered
+
+class BOWLoader(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        dictionary = gensim.corpora.dictionary.Dictionary.load('/home/bbales2/scraping/webpage/db/{0}.dictionary.big'.format(prefix))
+        for sentence in LoaderStopwords(self.clss):
+            yield dictionary.doc2bow(sentence)
+
+class Word2VecStopsLoader(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        notword = re.compile(r'[^\w -]')
+        for sentence in itertools.chain(*[session.query(cls).yield_per(100) for cls in self.clss]):
+            filtered = [word for word in stemSentence(sentence.string) if len(notword.sub('', word)) > 0 and len(word) < 75]
+            yield filtered
+
+class TFIDFLoader(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        tfidf = gensim.models.tfidfmodel.TfidfModel.load('/home/bbales2/scraping/webpage/db/{0}.tfidf.big'.format(prefix))
+        for sentence in BOWLoader(self.clss):
+            yield tfidf[sentence]
+
+class LSILoader(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        lsi = gensim.models.lsimodel.LsiModel.load('/home/bbales2/scraping/webpage/db/{0}.lsi.big'.format(prefix))
+        for sentence in TFIDFLoader(self.clss):
+            yield lsi[sentence]
+
+class Doc2VecStopsLoader(object):
+    def __init__(self, clss):
+        self.clss = clss
+
+    def __iter__(self):
+        notword = re.compile(r'[^\w -]')
+        for sentence in itertools.chain(*[session.query(cls).order_by(cls.id).yield_per(100) for cls in self.clss]):
+            filtered = [word for word in stemSentence(sentence.string) if len(notword.sub('', word)) > 0 and len(word) < 75]
+            yield gensim.models.doc2vec.LabeledSentence(words = filtered, tags = [sentence.id])
+
+#%%
+index2id = list(itertools.chain(*[[a[0] for a in session.query(cls).order_by(cls.id).with_entities(cls.id)] for cls in [mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]]))
+#%%
+with open('/home/bbales2/scraping/webpage/db/{0}.index2id.big'.format(prefix), 'w') as f:
+    f.write(json.dumps(index2id))
+#%%
+#%%
+#tmp = time.time()
+#doc2vec = gensim.models.doc2vec.Doc2Vec()
+#doc2vec.build_vocab(Doc2VecStopsLoader())
+#doc2vec.train(Doc2VecStopsLoader())
+#print 'Doc2Vec Training Time: ', time.time() - tmp
+#doc2vec.save('/home/bbales2/scraping/webpage/db/doc2vec.big')
+#%%
+#%%
+#corp = MyCorpus()
+
+prefix = 'body_abstract_figure.xml'
+
+## Build per-sentence statistics
+tmp = time.time()
+word2vec = gensim.models.word2vec.Word2Vec(Word2VecStopsLoader([mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]), workers = 4)
+word2vec.save('/home/bbales2/scraping/webpage/db/{0}.word2vec.big'.format(prefix))
+print 'Word2Vec Training Time: ', time.time() - tmp
+#word2vec = gensim.models.word2vec.Word2Vec.load('/home/bbales2/scraping/webpage/db/{0}.word2vec.big'.format(prefix))
+
+tmp = time.time()
+dictionary = gensim.corpora.dictionary.Dictionary(LoaderStopwords([mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]))
+dictionary.filter_extremes(no_below = 5, no_above = 0.5)
+dictionary.compactify()
+print 'Dictionary time: ', time.time() - tmp
+sys.stdout.flush()
+dictionary.save('/home/bbales2/scraping/webpage/db/{0}.dictionary.big'.format(prefix))
+
+import time
+tmp = time.time()
+tfidf = gensim.models.TfidfModel(BOWLoader([mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]))
+print 'Tfidf time: ', time.time() - tmp
+sys.stdout.flush()
+tfidf.save('/home/bbales2/scraping/webpage/db/{0}.tfidf.big'.format(prefix))
+
+#dictionary = gensim.corpora.dictionary.Dictionary.load('/home/bbales2/scraping/webpage/db/dictionary.fig.big')
+
+tmp = time.time()
+numberTopics = 300
+lsi = gensim.models.lsimodel.LsiModel(corpus = TFIDFLoader([mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]), num_topics = numberTopics, id2word = dictionary)
+print 'LSI Time: ', time.time() - tmp
+lsi.save('/home/bbales2/scraping/webpage/db/{0}.lsi.big'.format(prefix))
+sys.stdout.flush()
+
+tmp = time.time()
+index = gensim.similarities.docsim.Similarity('/home/bbales2/scraping/lsi_index/{0}.index.big'.format(prefix), LSILoader([mldb2.BodySentence, mldb2.AbstractSentence, mldb2.FigureSentence]), num_features = numberTopics)
+print 'Index Time: ', time.time() - tmp
+sys.stdout.flush()
+index.save('/home/bbales2/scraping/webpage/db/{0}.index.big'.format(prefix))
---
+++ mine_sentences2.py
@@ -0,0 +1,140 @@
+#%%
+import collections
+import json
+import lda
+import nltk
+import numpy
+import gensim
+import os
+import itertools
+import random
+import re
+import time
+import matplotlib.pyplot as plt
+import pattern.en, pattern.search
+import enchant
+import sklearn.cluster
+import scipy.cluster.hierarchy
+import stemming.porter2
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import lxml.etree
+import itertools
+import mldb2
+
+from miningTools import stemSentence
+
+f = open('/home/bbales2/scraping/electrochemistry/rawData', 'r')
+rawData = json.loads(f.read())
+f.close()
+
+base = "/home/bbales2/scraping/electrochemistry/data"
+import lxml.etree
+import itertools
+
+def dropns(root):
+    for node in root.iter():
+        if node.tag[0] == '{':
+            node.tag = node.tag.rsplit('}', 1)[-1]
+
+#%%
+session, engine = mldb2.getSession('/home/bbales2/scraping/electrochemistry/db.db')
+#%%
+worked = 0
+for i, fname in enumerate(rawData):
+    xmlF = os.path.join(base, fname, "xml")
+
+    if os.path.exists(xmlF):
+        doc = ''
+        with open(xmlF) as f:
+            doc = f.read()
+
+        etree = lxml.etree.fromstring(doc)
+
+        dropns(etree)
+
+        for e in itertools.chain(etree.findall( './/cross-ref' ),
+                                 etree.findall( './/cross-refs' ),
+                                etree.findall( './/formula' )):
+        #                            etree.findall( './/sup' ),
+        #                            etree.findall( './/hsp' ),
+        #                            etree.findall( './/label' ),
+        #                            etree.findall( './/inf' ),
+        #                            #etree.findall( './/italic' )
+            tail = e.tail
+            e.clear()
+            e.tail = tail
+        #     elem.getparent().remove(elem)
+
+        def get_text1(node):
+            result = node.text or ""
+            for child in node:
+                if child.tail is not None:
+                    result += child.tail
+            return result
+
+        def get_text3(node):
+            return (node.text or "") + "".join(
+                [etree.tostring(child) for child in node.iterchildren()])
+
+
+        attachments = {}
+        for attachment in etree.findall('.//attachment'):
+             attachments[attachment.findtext('file-basename')] = attachment.findtext('attachment-eid')
+
+        figures = []
+        for figure in etree.findall('.//figure'):
+            if len(figure.findall('.//figure')) == 0:
+                figures.append( { 'caption' : ''.join(list(figure.itertext())[1:]),
+                             'filename' : attachments[figure.find('link').get('locator')] } )
+
+        abstract = ' '.join([''.join(para.itertext()) for para in etree.findall('.//abstract-sec')])
+
+        doctext = []
+        for para in etree.findall('.//para'):
+            if len(para.findall('.//para')) == 0:
+                doctext.append(''.join(para.itertext()))
+
+        doctext = ' '.join(doctext)
+
+        if len(doctext) == 0:
+            continue
+
+        worked += 1
+
+        paperDb = session.query(mldb2.Paper).filter_by(name = fname).first()
+
+        if paperDb:
+            print 1/0
+
+        paperdb = mldb2.Paper(name = fname, data = rawData[fname])
+        session.add(paperdb)
+        session.commit()
+
+        abstractdb = mldb2.Abstract(paperId = paperdb.id)
+        session.add(abstractdb)
+        session.commit()
+
+        tree = pattern.en.parsetree(abstract, lemmata = True)
+        for j, sentence in enumerate(tree):
+            session.add(mldb2.AbstractSentence(loc = j, string = sentence.string, abstractId = abstractdb.id))
+        session.commit()
+
+        tree = pattern.en.parsetree(doctext, lemmata = True)
+        for j, sentence in enumerate(tree):
+            session.add(mldb2.BodySentence(loc = j, string = sentence.string, paperId = paperdb.id))
+        session.commit()
+
+        for figure in figures:
+            figdb = mldb2.Figure(filename = figure['filename'], paperId = paperdb.id)
+            session.add(figdb)
+            session.commit()
+
+            tree = pattern.en.parsetree(figure['caption'], lemmata = True)
+            for j, sentence in enumerate(tree):
+                session.add(mldb2.FigureSentence(loc = j, string = sentence.string, figureId = figdb.id))
+            session.commit()
+        #print figures
+        #print abstract
+        #print doctext
+    print '{0}/{1}'.format(i, len(rawData))
